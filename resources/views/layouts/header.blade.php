5968e8449a1bf50ed6cb18a33e3472ef040cb230
bruno <bruno.leonardo23@hotmail.com>
2018-05-16 00:35:55 -0300

    layout inicial do sistema

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+            SEATRAN
+            </a>
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="{{ url('/enviar') }}"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Enviar Arquivo</a>
+                </li>
+                <li>
+                    <a href="{{ url('/listar') }}"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Convênios</a>
+                </li>   
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <title>SEATRAN - {{ $title }}</title>
+        
+    </head>
+    <body>
+        <div class="row">
+			@include('layouts.header')
+		</div>
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				@yield('content')
+			</div>
+		</div>
+		
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
