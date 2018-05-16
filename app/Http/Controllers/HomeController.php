edbbeaa4a40e9382b4e73cbbf5829dc184950a59
bruno <bruno.leonardo23@hotmail.com>
2018-05-16 20:35:08 -0300

    criação do formulario de enviodo arquivo com validações e criação do metodo de envio do arquivo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Http\Model\Convenio;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -11,5 +13,31 @@ class HomeController extends Controller
 		$title = 'Enviar Arquivo';
 
 		return view('home', compact('title'));
+	}
+
+	public function setArquivo(Request $request)
+	{
+		$rules = [
+			'convenio' => 'mimes:xlsx,xls| required'
+		];
+
+		$messages = [
+		    'required' => 'Selecione o arquivo para o envio.',
+		    'mimes' => 'Tipo de aquivo não suportado.',
+		];
+
+		$validator = Validator::make($request->all(), $rules, $messages);
+
+		if ($validator->fails()) {
+
+			return redirect()->action('HomeController@index')
+		    ->with('class', 'danger')
+		    ->with('msg', 'Erro ao tentar enviar o arquivo!')
+		    ->withErrors($validator)
+		    ->withInput();
+
+		}
+
+		
 	}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,23 @@
 <div class="row">
     <div class="col-md-12">
         <h1>{{ $title }}</h1>
+        <hr>
     </div>
+</div>
+@include('layouts.msg')
+@include('layouts.erros')
+<div class="row">
+	<div class="col-md-12">
+		<form class="content" method="POST" action="{{ url('/submit') }}" enctype="multipart/form-data">
+			{{ csrf_field() }}
+			<div class="form-group">
+    			<label for="exampleInputEmail1">Arquivo Excel</label>
+    			<input type="file" class="form-control-file" id="convenio" type="file" name="convenio">
+  			</div>
+
+  			<input type="submit" name="enviar" value="Enviar" class="btn btn-success btn-lg">
+
+		</form>
+	</div>
 </div>
 @endsection
